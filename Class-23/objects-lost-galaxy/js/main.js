52d510cb23870ec04f45b0a077864ba7af1cde58
Marko <119257070+JakovMarko@users.noreply.github.com>
2023-08-15 22:29:12 +0200

    completed tony hawk obj

--- Class-23/objects-lost-galaxy/js/main.js
+++ Class-23/objects-lost-galaxy/js/main.js
@@ -1,1 +1,24 @@
 //Create a dog object that has four properties and three methods
+
+let dog = {
+    race: 'labradog',
+    color: 'white',
+    size: 'big',
+    age: '5',
+
+    woof: function(){
+        alert('WOOF!')
+    },
+
+    wiggle: function(){
+        alert("Your dog wiggles it's tail happily!")
+    },
+
+    catSpotted: function(){
+        alert("Cat spotted, engaging chase method protocol alpha, GROWLL!!!!!");
+    }
+}
+
+dog.woof();
+dog.wiggle();
+dog.catSpotted();
--- Class-23/objects-tony-hawk-pro-skater/js/main.js
+++ Class-23/objects-tony-hawk-pro-skater/js/main.js
@@ -1,1 +1,21 @@
 //Create a Tony Hawk Pro Skater constructor that makes fighting game characters with 4 properties and 3 methods
+
+function ProScater(firstName, lastName, skateColor, age, speed, balance, skill){
+    this.firstName = firstName;
+    this.lastName=lastName;
+    this.skateColor=skateColor;
+    this.age=age;
+
+    this.speed = function(){
+        console.log(`Your characters speed is: ${speed}`);
+    };
+
+    this.skill = function(){
+        console.log(`Your characters speed is: ${skill}`);
+    };
+
+    
+
+}
+
+let tony = new ProScater('Tony', 'Hawk', 'purple', 'old', 'not so fast any more', 'good', 'very skilled');
