7ba813fe014e2ec590ffe472734376dceb18ae4c
Marko <119257070+JakovMarko@users.noreply.github.com>
2023-08-15 21:58:29 +0200

    Finished space-review

--- Class-23/space-review/js/main.js
+++ Class-23/space-review/js/main.js
@@ -2,13 +2,30 @@
 
 //Create and array of numbers. Sum all of the numbers. Alert the sum.
 
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let sumArray = arr.reduce((total, item) => total+item,0);
+console.log(sumArray);
 //Create a function that takes in an array of numbers
 //Return a new array of numbers that is every original number squared
 
+function numbersSquared(arr){
+    return arr.map(item => Math.pow(item, 2));
+}
+console.log(numbersSquared([1,2,3,4,5,6,7]));
+
 //Create a function that takes string
 //Print the reverse of that string to the console
-
+function reverseStr(str){
+    return str.split('').reverse().join('');
+}
+console.log(reverseStr('jebaited'));
 //Create a function that takes in a string
 //Alert if the string is a palindrome or not
+function isAPalindrome(str){
+    let strReverse = str.split('').reverse().join('');
+    console.log(str);
+    return strReverse == str ? 'Is a palindrome' : 'Is not a palindrome'
+}
 
+console.log(isAPalindrome('hhhhh'));
 
--- Class-23/turbo-review/js/main.js
+++ Class-23/turbo-review/js/main.js
@@ -24,8 +24,10 @@ function rockPaperScissors(){
 function playRockPapperScissors(str){
     if((str == 'rock' && rockPaperScissors() == 'scissors') || (str == 'papper' && rockPaperScissors() == 'rock') || (str == 'scissors' && rockPaperScissors() == 'papper')){
         alert('You win!!!')
+    } else if(str == rockPaperScissors()){
+        alert('TIEDDDD!!!');
     } else {
-        alert('You lose!!!');
+        alert('YOU LOOOOSEE!')
     }
 }
 //Create a function that takes an array of choices. Play the game x times where x is the number of choices in the array. Print the results of each game to the console.
