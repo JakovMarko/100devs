07ae2a788d6c803f8b07730f9a411d95b0e6d0fd
Marko <119257070+JakovMarko@users.noreply.github.com>
2023-08-28 21:45:55 +0200

    done class 26

--- Class-26/objects-shots/js/main.js
+++ Class-26/objects-shots/js/main.js
@@ -1,10 +1,23 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click', getDrink);
 
-fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a")
+function getDrink(){
+    let drink = document.querySelector('input').value;
+
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s= ${drink}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
+        console.log(data.drinks);
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+        document.querySelector('h2').textContent = data.drinks[0].strDrink;
+        document.querySelector('h3').textContent = data.drinks[0].strInstructions;
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
+
+}
+
+
+
+
