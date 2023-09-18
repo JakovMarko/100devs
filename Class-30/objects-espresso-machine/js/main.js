b33b61b78d51fce03e3bd89b313a5cf3e3091d8c
Marko <119257070+JakovMarko@users.noreply.github.com>
2023-09-18 22:19:33 +0200

    done with some homework

--- Class-30/objects-espresso-machine/js/main.js
+++ Class-30/objects-espresso-machine/js/main.js
@@ -1,1 +1,40 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+// class EspressoMachine{
+//     constructor(espressoType, espressoSize, espressoColor, espressoSpeed, working = false){
+       
+//         this.type = espressoType
+//         this.size = espressoSize
+//         this.color = espressoColor
+//         this.speed = espressoSpeed
+//     }
+    
+//     machineStart(){
+//         if(!working){
+//             console.log("Machine is starting!");
+//             working = true;
+            
+//         } 
+//     }
+// }
+// let esresso = new EspressoMachine('ayayay', 'medium', 'red', 'super fast');
+
+
+class Account{
+    constructor(name){
+        this.name = name;
+        this.balance = 0;
+    }
+    credit(value){
+        return this.balance += value;
+    };
+
+    describe(){
+        return `owner: ${this.name}, balance: ${this.balance}`;
+    }
+}
+
+const storage = [];
+storage.push(new Account('Sean'), new Account('Brad'), new Account('Georges'));
+console.log(storage);
+
+storage.forEach(account => account.credit(1000));
